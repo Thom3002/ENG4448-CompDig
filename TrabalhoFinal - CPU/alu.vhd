46df1cb0833962c1c2786ceaf432718d01fbe634
Thomas de Mello Oliva <thomas.mello301@gmail.com>
2025-07-05 13:50:36 -0300

    implementa operadores aritmeticos na alu

--- TrabalhoFinal - CPU/alu.vhd
+++ TrabalhoFinal - CPU/alu.vhd
@@ -1,20 +1,20 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    10:51:43 07/04/2025 
--- Design Name: 
--- Module Name:    alu - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
+-- Company:
+-- Engineer:
 --
--- Dependencies: 
+-- Create Date:    10:51:43 07/04/2025
+-- Design Name:
+-- Module Name:    alu - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool versions:
+-- Description:
 --
--- Revision: 
+-- Dependencies:
+--
+-- Revision:
 -- Revision 0.01 - File Created
--- Additional Comments: 
+-- Additional Comments:
 --
 ----------------------------------------------------------------------------------
 library IEEE;
@@ -22,7 +22,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
@@ -30,14 +30,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity alu is
-    generic (
-        N : positive := 8                    -- largura
-    );
     port (
-        A        : in  unsigned(N-1 downto 0);
-        B        : in  unsigned(N-1 downto 0);
+        A        : in  unsigned(7 downto 0);
+        B        : in  unsigned(7 downto 0);
         CMD      : in  std_logic_vector(3 downto 0);
-        R        : out unsigned(N-1 downto 0);
+        R        : out unsigned(7 downto 0);
         ZERO     : out std_logic;
         NEGATIVE : out std_logic;
         OVERFLOW : out std_logic;
@@ -48,9 +45,53 @@ entity alu is
 end alu;
 
 architecture Behavioral of alu is
-
+    -- signals
+    signal temp_R : unsigned(7 downto 0);
+    signal temp_OVERFLOW : std_logic;
 begin
 
+    process (A, B, CMD)
+
+    variable sum_sub_res : unsigned(8 downto 0); -- 9 bits to handle overflow in addition/subtraction
+    begin
+
+        -- default values to avoid latches
+        temp_R := (others => '0');
+        ZERO     <= '0';
+        NEGATIVE <= '0';
+        OVERFLOW <= '0';
+        EQUAL    <= '0';
+        GREATER  <= '0';
+        SMALLER  <= '0';
+        temp_OVERFLOW <= '0';
+
+        case CMD is
+            when "0000" => -- ADD
+                sum_sub_res := ("0" & A) + ("0" & B); -- Faz a conta com 9 bits para evitar overflow
+                temp_R <= sum_sub_res(7 downto 0);
+                temp_OVERFLOW <= sum_sub_res(8);
+
+            when "0001" => -- SUB
+                sum_sub_res := ("0" & A) - ("0" & B);
+                temp_R <= sum_sub_res(7 downto 0);
+                temp_OVERFLOW <= sum_sub_res(8);
+
+            -- when "0010" => -- AND
+            --     temp_R <= A and B;  -- AND
+            --     temp_OVERFLOW <= '0';
+            -- when "0011" => -- OR
+            --     temp_R <= A or B;  -- OR
+            --     temp_OVERFLOW <= '0';
+
+            -- when "0100" => temp_R <= not A;    -- NOT
+            when others =>
+                temp_R <= (others => '0');
+                temp_OVERFLOW <= '0';
+        end case;
+
+        R <= temp_R;
+        OVERFLOW <= temp_OVERFLOW;
+
+    end process;
 
 end Behavioral;
-
--- TrabalhoFinal - CPU/cpu.vhd
+++ TrabalhoFinal - CPU/cpu.vhd
@@ -18,16 +18,25 @@ ARCHITECTURE Behavioral OF CPU IS
 	TYPE cpu_fsm_t IS (st_idle, st_fetch, st_decode, st_execute, st_write);
 	SIGNAL state : cpu_fsm_t := st_idle;
 
+	-- UC registers
 	SIGNAL PC, IR, SP, MAR : unsigned(7 DOWNTO 0) := (OTHERS => '0');
- 
+
 	TYPE reg_array IS ARRAY(0 TO 3) OF std_logic_vector(7 DOWNTO 0);
 	SIGNAL registers : reg_array := (
-		0 => "10000000", 
+		0 => "10000000",
 		OTHERS => (OTHERS => '0')
 	);
 
+	-- ALU signals
 	SIGNAL ALU_A, ALU_B, ALU_CMD, ALU_R : unsigned(7 DOWNTO 0) := (OTHERS => '0');
-	
+
+	SIGNAL alu_zero   : STD_LOGIC;
+    SIGNAL alu_neg    : STD_LOGIC;
+    SIGNAL alu_ovf    : STD_LOGIC;
+    SIGNAL alu_eq     : STD_LOGIC;
+    SIGNAL alu_gt     : STD_LOGIC;
+    SIGNAL alu_lt     : STD_LOGIC;
+
 	TYPE ALU_FLAGS_t IS RECORD
 		zero : std_logic;
 		negative : std_logic;
@@ -36,45 +45,37 @@ ARCHITECTURE Behavioral OF CPU IS
 		greater : std_logic;
 		smaller : std_logic;
 	END RECORD ALU_FLAGS_t;
-	
+
 SIGNAL ALU_FLAGS : ALU_FLAGS_t := (OTHERS => '0');
 BEGIN
 	-- ALU
 	U_ALU : ENTITY work.alu
 		PORT MAP(
-			A => ALU_A, 
-			B => ALU_B, 
-			CMD => std_logic_vector(ALU_CMD(3 DOWNTO 0)), 
-			R => ALU_R, 
-			ZERO => alu_zero, 
-			NEGATIVE => alu_neg, 
-			OVERFLOW => alu_ovf, 
-			EQUAL => alu_eq, 
-			GREATER => alu_gt, 
+			A => ALU_A,
+			B => ALU_B,
+			CMD => std_logic_vector(ALU_CMD(3 DOWNTO 0)),
+			R => ALU_R,
+			ZERO => alu_zero,
+			NEGATIVE => alu_neg,
+			OVERFLOW => alu_ovf,
+			EQUAL => alu_eq,
+			GREATER => alu_gt,
 			SMALLER => alu_lt
 		);
 
-		ALU_FLAGS <= (
-			zero => alu_zero, 
-			negative => alu_neg, 
-			overflow => alu_ovf, 
-			equal => alu_eq, 
-			greater => alu_gt, 
-			smaller => alu_lt
-			);
- 
+
 		-- LCD
 		U_LCD : ENTITY work.lcd
 			PORT MAP(
-				clk => CLK, 
-				reset => RESET, 
-				lcd_rs => lcd_rs_s, 
-				lcd_rw => lcd_rw_s, 
-				lcd_e => lcd_e_s, 
-				lcd_data => lcd_data_s, 
+				clk => CLK,
+				reset => RESET,
+				lcd_rs => lcd_rs_s,
+				lcd_rw => lcd_rw_s,
+				lcd_e => lcd_e_s,
+				lcd_data => lcd_data_s,
 				SF_CE0 => sf_ce0_s
 			);
-	
+
 	PROCESS (CLK)
 	BEGIN
 		IF rising_edge(clk) THEN
@@ -82,7 +83,7 @@ BEGIN
 				state <= st_idle;
 			ELSE
 				CASE state IS
-					WHEN st_idle => 
+					WHEN st_idle =>
 						PC <= to_unsigned(0, PC'length); -- Program Counter
 						IR <= to_unsigned(0, IR'length); -- Instruction Register
 						SP <= to_unsigned(254, SP'length); -- Stack Pointer
@@ -90,20 +91,20 @@ BEGIN
 						IF reset = '0' THEN
 							state <= st_fetch;
 						END IF;
- 
-					WHEN st_fetch => 
+
+					WHEN st_fetch =>
 						RAM_WE <= '0';
 						IR <= unsigned(RAM_DOUT);
 
 						state <= st_decode;
- 
-					WHEN st_decode => 
+
+					WHEN st_decode =>
 						-- add Rx, Ry "0000 Rx Ry" Rx ? Rx + Ry; PC ? PC + 1
 						IF IR(7 DOWNTO 4) = "0000" THEN
-							ALU_A <= registers(to_integer(IR(3 DOWNTO 2)));  
-							ALU_B <= registers(to_integer(IR(1 DOWNTO 0)));  
+							ALU_A <= registers(to_integer(IR(3 DOWNTO 2)));
+							ALU_B <= registers(to_integer(IR(1 DOWNTO 0)));
 							ALU_CMD <= "0000"; -- add
- 
+
 						state <= st_execute;
 						-- sub Rx,Ry "0001 Rx Ry" Rx ← Rx − Ry
 						ELSIF IR(7 DOWNTO 4) = "0001" THEN
@@ -116,7 +117,7 @@ BEGIN
 							ALU_A <= unsigned(registers(to_integer(IR(3 DOWNTO 2))));
 							IF IR(1 DOWNTO 0) = "00" THEN -- inc
 								ALU_B <= to_unsigned(1, 8); -- 00000001 (incrementa 1)
-								ALU_CMD <= "0000"; -- add 
+								ALU_CMD <= "0000"; -- add
 							ELSE -- dec
 								ALU_B <= to_unsigned(1, 8);
 								ALU_CMD <= "0001"; -- sub
@@ -151,7 +152,7 @@ BEGIN
 							 ALU_A   <= registers(to_integer(IR(3 downto 2)));
 							 ALU_B   <= ("000000" & IR(1 downto 0));
 							 ALU_CMD <= "0111";
-						
+
 						-- push / pop / st / ld   "1000 Rx ss"
 						ELSIF IR(7 DOWNTO 4) = "1000" THEN
 							 -- push Rx  ss = 00
@@ -192,32 +193,37 @@ BEGIN
 							 RAM_DIN  <= registers(to_integer(IR(3 DOWNTO 2))); -- dado = Rx
 							 RAM_WE   <= '1';                                   -- escrita
 							 state    <= st_write;
-						
-						
-						END IF;
-						
-					WHEN st_execute => 
-						PC <= PC + 1;
-						MAR <= MAR + 1;
-						state <= st_fetch;
-						IF IR(7 DOWNTO 4) = "0000" THEN
-							registers(to_integer(IR(3 DOWNTO 2))) <= ALU_R;
-							-- flags
-						ELSIF IR = "1111_1111" THEN
-							PC <= PC;
-							MAR <= PC;
-						END IF;
- 
 
-					WHEN st_write => 
-						RAM_WE <= '0';
-						IF IR(7 DOWNTO 2) = "100000" THEN      -- push
-						SP <= SP - 1;
-						ELSIF IR(7 DOWNTO 2) = "100001" THEN   -- pop
-							registers(to_integer(IR(3 DOWNTO 2))) <= RAM_DOUT;
-							SP <= SP + 1;
-							PC <= PC + 1;             -- push/pop/ldr/str
-							state <= st_fetch;
+
+						END IF;
+
+					WHEN st_execute =>
+
+						-- Copia o resultado da ALU para o registrador de destino
+						IF IR(7 DOWNTO 4) >= "0000" AND IR(7 DOWNTO 4) <= "0111" THEN -- Todas as instruções ALU
+							registers(to_integer(IR(3 DOWNTO 2))) <= ALU_R;
+							ALU_FLAGS.zero     <= alu_zero;
+							ALU_FLAGS.negative <= alu_neg;
+							ALU_FLAGS.overflow <= alu_ovf;
+							ALU_FLAGS.equal    <= alu_eq;
+							ALU_FLAGS.greater  <= alu_gt;
+							ALU_FLAGS.smaller  <= alu_lt;
+						END IF;
+
+						PC <= PC + 1;
+
+						state <= st_fetch;
+
+
+					WHEN st_write =>
+						-- RAM_WE <= '0';
+						-- IF IR(7 DOWNTO 2) = "100000" THEN      -- push
+						-- SP <= SP - 1;
+						-- ELSIF IR(7 DOWNTO 2) = "100001" THEN   -- pop
+						-- 	registers(to_integer(IR(3 DOWNTO 2))) <= RAM_DOUT;
+						-- 	SP <= SP + 1;
+						-- 	PC <= PC + 1;             -- push/pop/ldr/str
+						-- 	state <= st_fetch;
 				END CASE;
 			END IF;
 		END IF;
