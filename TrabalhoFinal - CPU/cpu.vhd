1a7800ec919446503f1ada04841ccabb92adfe7a
Thomas de Mello <thomas.mello301@gmail.com>
2025-07-06 16:49:05 -0300

    fix: adiciona ;

--- TrabalhoFinal - CPU/cpu.vhd
+++ TrabalhoFinal - CPU/cpu.vhd
@@ -176,7 +176,7 @@ BEGIN
 
 							 -- pop Rx   ss = 01
 							 ELSIF IR(1 DOWNTO 0) = "01" THEN
-								  registers(to_integer(IR(3 DOWNTO 2))) <= DOUT
+								  registers(to_integer(IR(3 DOWNTO 2))) <= RAM_DOUT;
 								  MAR      <= SP + 1;        -- SP + 1 primeiro
 								  RAM_WE   <= '0';                            -- leitura
 								  state    <= st_execute;                       -- captura dado + SP++
@@ -190,7 +190,7 @@ BEGIN
 
 							 -- ld Rx, ADDR   ss = 11
 							 ELSE         
-							 	  registers(to_integer(IR(3 DOWNTO 2))) <= DOUT                                    -- "11"
+							 	  registers(to_integer(IR(3 DOWNTO 2))) <= RAM_DOUT;                                   -- "11"
 								  MAR      <= PC + 1;        -- lê ADDR
 								  RAM_WE   <= '0';                            -- leitura
 								  state    <= st_execute;                       -- traz dado para Rx
@@ -199,7 +199,7 @@ BEGIN
 						-- ldr Rx, [Ry]   "1001 Rx Ry"
 						ELSIF IR(7 DOWNTO 4) = "1001" THEN
 							 MAR      <= registers(to_integer(IR(1 DOWNTO 0))); -- endereço = Ry
-							 registers(to_integer(IR(3 DOWNTO 2))) <= DOUT;                                 -- "11"
+							 registers(to_integer(IR(3 DOWNTO 2))) <= RAM_DOUT;                                 -- "11"
 							 RAM_WE   <= '0';                                   -- leitura
 							 state    <= st_execute;                              -- copia em Rx
 
@@ -219,7 +219,7 @@ BEGIN
 						elsif IR(7 downto 4) = "1100" then
 							-- JMP 0x-- --> pc <-- MEM[PC+1]
 							if IR(1 downto 0) = "00" then
-								MAR <= STD_LOGIC_VECTOR(unsigned(PC)+1)
+								MAR <= STD_LOGIC_VECTOR(unsigned(PC)+1);
 							elsif IR(1 downto 0) = "01" then
 								MAR <= registers(to_integer(IR(3 DOWNTO 2))); 
 
@@ -245,25 +245,25 @@ BEGIN
 								if ALU_FLAGS(2) = '1' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							elsif IR(1 downto 0) = "01" then
 								if ALU_FLAGS(2) = '0' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							elsif IR(1 downto 0) = "10" then
 								if ALU_FLAGS(3) = '1' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							elsif IR(1 downto 0) = "11" then
 								if ALU_FLAGS(3) = '0' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							end if;
 							state    <= st_execute;
@@ -273,19 +273,19 @@ BEGIN
 								if ALU_FLAGS(4) = '1' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							elsif IR(1 downto 0) = "01" then
 								if ALU_FLAGS(5) = '1' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							elsif IR(1 downto 0) = "10" then
 								if ALU_FLAGS(1) = '1' then
 									MAR <= registers(to_integer(IR(3 downto 2)));
 								else 
-									PC <= PC+1
+									PC <= PC+1;
 								end if;
 							end if;
 							state    <= st_execute;
@@ -293,7 +293,8 @@ BEGIN
 							if IR(3 downto 0) = "0000" then
 								PC <= PC+1;
 							elsif IR(3 downto 0) = "1111" then	
-							state    <= st_execute;
+								state    <= st_execute;
+							end if;
 						end if;
 							
 					WHEN st_execute =>
@@ -366,6 +367,7 @@ BEGIN
 						elsif IR(7 downto 4) = "1111" then
 							if IR(3 downto 0) = "1111" then	
 								MAR<=PC;
+							end if;
 								
 						END IF;
 
